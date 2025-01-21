0df2a157225633d4545e3a405387d6350f5e3967
Evelyn <evelyn.neves89@gmail.com>
2025-01-20 23:46:27 -0300

    fix: storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,22 +10,20 @@
 
 module.exports = {
     stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
         "@chromatic-com/storybook"
     ],
-
     framework: {
         name: "@storybook/nextjs",
         options: {}
     },
-
     docs: {},
-
     typescript: {
         reactDocgen: "react-docgen-typescript"
-    }
+    },
+    staticDirs: ["../public"],
 };
+
